986beb0321df1ecd6c7c83e33a4baca2fe766e50
Thamily <thamilyp0@gmail.com>
2026-03-23 20:17:55 -0300

    Add files via upload

---
+++ ex1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+    <form method="POST">
+        Número: <input type="number" name="n">
+        <input type="submit" value="Enviar">
+    </form>
+<?php
+if(isset($_POST["n"])){
+    $n=$_POST["n"];
+    $i=1;
+    while($i <=$n){
+        echo $i. "<br>";
+        $i++;
+    }
+}
+?>
+</body>
+</html>
